effb93dc26a0df880afca04ae5904fb3b8b9b1cd
williamhuangv <13860669931@qq.com>
2026-02-04 10:12:52 +0800

    Simplify app.py

--- app.py
+++ app.py
@@ -4,27 +4,19 @@ import psycopg2
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    """首页 - 测试数据库连接"""
     try:
         with psycopg2.connect(Config.DB_URI) as conn:
             with conn.cursor() as cursor:
                 cursor.execute("SELECT version(), current_database();")
                 version, db_name = cursor.fetchone()
 
-                return render_template('index.html',
-                                      success=True,
-                                      db_name=db_name,
-                                      version=version[:100] + '...')
+                return render_template('index.html',success=True,db_name=db_name,version=version[:100] + '...')
 
     except Exception as e:
-        return render_template('index.html',
-                              success=False,
-                              error_type=type(e).__name__,
-                              error_detail=str(e))
-
+        return render_template('index.html',success=False,error_type=type(e).__name__,error_detail=str(e))
 
 if __name__ == '__main__':
     app.run(debug=True)
+    

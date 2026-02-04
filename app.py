3efcbd08611b55957623790325e42f50d4b68456
williamhuangv <13860669931@qq.com>
2026-02-04 09:52:28 +0800

    Simplify app.py with context manager

--- app.py
+++ app.py
@@ -1,36 +1,30 @@
 from flask import Flask, render_template
+from config import Config
+import psycopg2
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     """首页 - 测试数据库连接"""
-    import psycopg2
-    from config import Config
-
     try:
-        conn = psycopg2.connect(Config.DB_URI)
-        cursor = conn.cursor()
+        with psycopg2.connect(Config.DB_URI) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT version(), current_database();")
+                version, db_name = cursor.fetchone()
 
-        cursor.execute("SELECT version();")
-        version = cursor.fetchone()
-
-        cursor.execute("SELECT current_database();")
-        db_name = cursor.fetchone()
-
-        cursor.close()
-        conn.close()
-
-        return render_template('index.html',
-                              success=True,
-                              db_name=db_name[0],
-                              version=version[0][:100] + '...')
+                return render_template('index.html',
+                                      success=True,
+                                      db_name=db_name,
+                                      version=version[:100] + '...')
 
     except Exception as e:
         return render_template('index.html',
                               success=False,
                               error_type=type(e).__name__,
                               error_detail=str(e))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
